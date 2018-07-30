d82a2617c6adc4071e9bce7dd31059262383bf3b
Santos Gonzalez <sgonzo3@gmail.com>
2018-07-29 23:00:43 -0400

    feat: project complete

--- designs.js
+++ designs.js
@@ -1,8 +1,37 @@
-// Select color input
-// Select size input
-
+const color = $('#colorPicker');
+const height = $('#inputHeight');
+const width = $('#inputWidth');
+const canvas = $('#pixelCanvas');
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
-  // Your code goes here!
-}
+  // Creates rows for the grid
+  for (let x = 1; x <= height.val(); x++) {
+    canvas.append('<tr></tr>');
+  };
+  // Creates squares of the grid using a while loop to satisfy the project rubric
+  let y = 1
+  while (y <= width.val()) {
+    $('tr').append('<td></td>');
+    y++;
+  };
+};
+
+// Creates an event listener for user submission
+$('#sizePicker').submit(function(event) {
+//   Erase any previously drawn grid
+  $('#pixelCanvas').empty();
+//   Draws new Grid
+  makeGrid();
+  event.preventDefault();
+});
+
+// Creates an event listener for user clicks on the canvas, changes color of tile
+canvas.on('click', 'td', function(event) {
+  let chosenColor = color.val();
+  event.preventDefault();
+  if ($(this).css('background-color') != 'rgb(227, 227, 227)') {
+    $(this).css('background-color', 'rgb(227, 227, 227)');
+  } else {
+    $(this).css('background-color', chosenColor);
+  }
+});
